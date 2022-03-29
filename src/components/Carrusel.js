d0cc428e7be4ec76d66fdbfcb8a46573ee92b9de
Peter Aspiazu <paspiazusabando@gmail.com>
2022-03-28 21:49:45 -0500

    Actualizando formulario

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,10 +1,9 @@
 // import required modules
-import { Lazy, Pagination } from "swiper";
+import { Pagination } from "swiper";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
-import "swiper/css/lazy";
 import "swiper/css/pagination";
 
 
@@ -17,7 +16,6 @@ export const Carrusel = () => {
                 // install Swiper modules
                 slidesPerView={1}
                 spaceBetween={10}
-                lazy={true}
                 pagination={{
                 clickable: true,
                 }}
@@ -35,29 +33,26 @@ export const Carrusel = () => {
                     spaceBetween: 50,
                 },
                 }}
-                modules={[Lazy, Pagination]}
+                modules={[Pagination]}
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src="./assets/img/ui.jpg" alt="Interfaz de usuario" title="Interfaz de usuario" className="swiper-lazy" />
+                    <img src="./assets/img/ui.jpg" alt="Interfaz de usuario" title="Interfaz de usuario" />
                     <div className="swiper__div">
                         <p>Interfaz de usuario</p>
                     </div>
-                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="./assets/img/js.jpg" alt="Código limpio" title="Código limpio" className="swiper-lazy" />
+                    <img src="./assets/img/js.jpg" alt="Código limpio" title="Código limpio" />
                     <div className="swiper__div">
                         <p>Código limpio</p>
                     </div>
-                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src="./assets/img/ux.jpg" alt="Experiencia de usuario" title="Experiencia de usuario" className="swiper-lazy" />
+                    <img src="./assets/img/ux.jpg" alt="Experiencia de usuario" title="Experiencia de usuario" />
                     <div className="swiper__div">
                         <p>Experiencia de usuario</p>
                     </div>
-                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
                 </SwiperSlide>
             </Swiper>
         </div>
